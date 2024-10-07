06aa2f94a524fea5ff4cf87d6389c0f74546c24e
Mitch V <=mvandy33@gmail.com>
2024-10-07 13:49:22 -0600

    Stubbed out first BR pages

---
+++ page/baseball-reference/header.ts
@@ -0,0 +1,7 @@
+import PageObject from "../abstract/page-object";
+
+export default class Header extends PageObject {
+    constructor(locator: string) {
+        super();
+    }
+}
---
+++ page/baseball-reference/home.ts
@@ -0,0 +1,10 @@
+import PageObject from "../abstract/page-object";
+
+export default class Home extends PageObject {
+
+    locator: string;
+
+    constructor() {
+        super();
+    }
+}
