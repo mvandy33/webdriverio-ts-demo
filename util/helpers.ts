416d75909ae8c72672614ceb3e35cae816ef7ef1
Mitch V <=mvandy33@gmail.com>
2024-10-11 16:25:42 -0600

    Implemented ad removal before each interaction to speed execution

--- util/helpers.ts
+++ util/helpers.ts
@@ -5,6 +5,17 @@ import * as _ from 'lodash';
  */
 export async function navigate() {
     await browser.url(process.env.ENVIRONMENT === 'test' ? '/app/' : '/');
+    await removeAds();
+}
+
+export async function removeAds() {
+    await browser.execute(`
+        ads = Array.from(document.querySelectorAll('[class*="adblock"]'));
+        ads = ads.concat(Array.from(document.querySelectorAll('[id*="google_ads"]')));
+        for (let ad of ads){
+            ad.remove();
+        }
+    `, []);
 }
 
 /**
@@ -20,6 +31,12 @@ export async function clear(input: WebdriverIO.Element) {
     await input.addValue(backstring);
 }
 
+/**
+ * Compare two object for equality
+ * @param actual 
+ * @param expected 
+ * @returns Empty string if equal - string with object descriptions if not equal
+ */
 export function compare(actual: object, expected: object) {
     let pruned = prune(actual, expected);
     if (!_.isEqual(pruned.result1, pruned.result2)) {
@@ -28,6 +45,12 @@ export function compare(actual: object, expected: object) {
     return '';
 }
 
+/**
+ * Get two objects that have only common properties from the initial inputs
+ * @param obj1 
+ * @param obj2 
+ * @returns 
+ */
 function prune(obj1: object, obj2: object) {
     let sharedKeys = Object.keys(obj1).filter(key => Object.keys(obj2).includes(key));
     return {
